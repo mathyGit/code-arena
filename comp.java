fb1f5cdb0b0f6e249620c81da52e3cb91fc336fa
mathyGit <shanmathyinfo@gmail.com>
2016-08-23 17:54:24 +0530

    Create comp.java

---
+++ comp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String s1=sc.nextLine();
+    String s2=sc.nextLine();
+    if(s1.equals(s2)){
+    	System.out.print("Strings are equal");
+    }
+    else System.out.print("Unequal strings");
+  }
+}
